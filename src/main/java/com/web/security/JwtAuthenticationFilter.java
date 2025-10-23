79e47f99d78c89aef281fb4b06b40083fd4e1ac6
quitedob <dobqop999@gmail.com>
2025-10-23 20:17:05 +0800

    version 0.1

--- src/main/java/com/web/security/JwtAuthenticationFilter.java
+++ src/main/java/com/web/security/JwtAuthenticationFilter.java
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
